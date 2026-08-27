6c3928295deb77e15b219ef79203fb34bb8f25a6
0Yayo0 <osvaldoss757@gmail.com>
2026-08-27 13:14:22 -0400

    Create Persona.java

---
+++ src/Persona.java
@@ -0,0 +1,50 @@
+public abstract class Persona
+/* 
+abstract porque en nuestro sistema "no existen" personas sin un rol (son cliente o encargado) 
+entonces, se restringe la creacion de personas con un new Persona (eso queda en manos de las sub clases)
+*/
+{
+    private String nombre;
+    private String rut;
+
+// constructores
+
+    public Persona()
+    {
+        nombre = "";
+        rut = "";
+    }
+
+    public Persona(String nombre, String rut)
+    {
+        this.nombre = nombre;
+        this.rut = rut;
+    }
+
+//metodos
+
+    public abstract void identificarse(); //cada sub clase debe implementar (o no, dependiendo si se convierte en una super) "su propio" identificarse
+
+//getter y setters
+
+    public String getNombre()
+    {
+        return nombre;
+    } 
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getRut()
+    {
+        return rut;
+    }
+
+    public void setRut(String rut){
+        //en caso de errores de tipeo, aunque no se si dejarlo
+        this.rut = rut;
+    }
+
+
+}
